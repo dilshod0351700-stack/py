e4024854b25c6d155bd3c48f7505dd71ba522280
dilshod0351700-stack <dilshod0351700@gmail.com>
2025-12-12 16:19:35 +0500

    Add files via upload

---
+++ birnechaAmaliyot3.py
@@ -0,0 +1,10 @@
+son_1=float(input('Birinchi sonni kiriting - '))
+son_2=float(input('Ikkinchi sonni kiriting - '))
+x=son_1
+y=son_2
+if x>y:
+    print(f'{x}>{y}')
+elif x<y:
+    print(f'{x}<{y}')
+elif x==y:
+    print(f'{x}={y}')
